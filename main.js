f33202ea1081a98449effc71e14c00b8e524281d
chaitu4203 <87125705+chaitu4203@users.noreply.github.com>
2021-12-23 06:26:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+canvas = 
+document.getElementById("mycanvas");
+ctx = canvas.getContext("2d");
+
+ctx.beginpth();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.strokeStyle();
